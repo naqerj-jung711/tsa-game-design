8f74ec0e604a392bdb78962d96f8a315840649af
naqerj-jung711 <andrewjung0711@gmail.com>
2022-02-21 22:28:53 -0500

    added animations/transitions between scenes
    tested between start and 1st level
    will be implemented properly tomorrow.

---
+++ [TSA SLC] Shape Avoider/Assets/fade.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class fade : MonoBehaviour
+{
+    public bool yes;
+    public Animator transition;
+    public float waittime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (yes == true)
+        {
+            LoadNext();
+        }    
+     
+    }
+    public void Set()
+    {
+        yes = true;
+    }
+
+    public void LoadNext()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    } 
+    
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("circlewipe");
+        yield return new WaitForSeconds(waittime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
